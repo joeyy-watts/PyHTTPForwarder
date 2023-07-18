54e9b974e586339f7b940d74d4571a463be24466
joeyy_watts <joeyms40@gmail.com>
2023-07-18 14:59:32 +0700

    return target status and result to caller

--- http_forwarder_server.py
+++ http_forwarder_server.py
@@ -15,7 +15,6 @@ class PyHTTPForwarderHandler(BaseHTTPRequestHandler):
 
     def __forward_http(self):
         # TODO: support other HTTP methods
-        # TODO: return result from target to caller
         try:
             result = requests.get("http://" + self.local_ip_handler.local_ip + self.path)
 
@@ -23,19 +22,20 @@ class PyHTTPForwarderHandler(BaseHTTPRequestHandler):
                 print(f"Target returned non-OK status code, updating local IP and retrying..")
                 self.local_ip_handler.refresh_local_ip()
                 result = requests.get("http://" + self.local_ip_handler.local_ip + self.path)
+
                 if result.status_code != 200:
-                    print(f"Failed to forward")
+                    print(f"Target returned on OK status code :: {result.status_code}")
+                else:
+                    print(f"Target returned OK, returning result to caller..")
 
-            print(f"forwarded, result is :: {result}")
+                self.send_response(result.status_code)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(bytes(result.text, "utf-8"))
+                return result
         except Exception as e:
-            print(f"Failed to forward to {self.local_ip_handler.local_ip}{self.path}")
+            print(f"Failed to forward to {self.local_ip_handler.local_ip}{self.path}, due to exception :: {e}")
             raise Exception(e)
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html>OK</html>", "utf-8"))
-        return result
 
     def do_GET(self):
         print(f"got request at path :: {self.path}")
